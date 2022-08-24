48596640dae98de399238d9469ed7a1ba9092c99
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-08-24 13:27:03 +0200

    pushing

--- exercises/src/index.js
+++ exercises/src/index.js
@@ -1,11 +1,16 @@
 import { createRoot } from 'react-dom/client';
-import { HelloWorld } from './HelloWorld';
 
-const helloWorldElement = <HelloWorld />
 const idRoot = document.querySelector('#root')
 const root = createRoot(idRoot)
 
+import { HelloWorld } from './HelloWorld';
+const helloWorldElement = <HelloWorld />
+
 root.render(helloWorldElement)
 
+import { HelloName } from './HelloName';
+const helloNameElement = <HelloName />
+
+root.render(helloNameElement)
 // ReactDOM.render it's DEPRECATED
 // ReactDOM.render(helloWorldElement, root)
