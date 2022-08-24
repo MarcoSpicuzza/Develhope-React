f6de295938d0268cfb397ef7b19d925e154f39a0
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-08-24 15:31:51 +0200

    Exercise JSX_05 done

--- exercises/src/HelloName.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const HelloName = (name) => {
-  name = "Marco";
-  return <h1>Hello, {name}</h1>;
-}
--- exercises/src/index.js
+++ exercises/src/index.js
@@ -1,14 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import { HelloWorld } from './HelloWorld';
-import { HelloName } from './HelloName';
+import { Sum } from './Sum';
 
 const idRoot = document.querySelector('#root')
 const root = createRoot(idRoot)
 
 root.render(
     <>
-        <HelloWorld />
-        <HelloName />
+        <Sum />
     </>
 )
 
