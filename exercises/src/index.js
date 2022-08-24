8b19a483062497e7f7932f1d62115832c9bf1ee1
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-08-24 12:22:49 +0200

    updating code and deleting not used files

--- exercises/src/index.js
+++ exercises/src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import { HelloWorld } from './HelloWorld';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const helloWorldElement = <HelloWorld />
+const idRoot = document.querySelector('#root')
+const root = createRoot(idRoot)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(helloWorldElement)
+
+// ReactDOM.render it's DEPRECATED
+// ReactDOM.render(helloWorldElement, root)
