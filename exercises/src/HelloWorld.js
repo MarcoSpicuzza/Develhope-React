8b209e0239287c530842e9ff2dd4dd2e3cb1bdc2
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-08-25 22:37:13 +0200

    Exercise Components_07 done

--- exercises/src/HelloWorld.js
+++ exercises/src/HelloWorld.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Message } from "./Message.js";
 
 export class HelloWorld extends React.Component {
     render() {
         return (
             <>
                 <h1>Hello World!</h1>
-                <p>What a beautiful day!</p>
+                <Message />
             </>
         );
     }
