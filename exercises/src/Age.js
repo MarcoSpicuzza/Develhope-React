b4f39ebe6c3fd89435ab6dcb9bfb2e7c0cd374bb
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-08-31 12:09:07 +0200

    Exercise Conditional-Rendering_4 done

--- exercises/src/Age.js
+++ exercises/src/Age.js
@@ -5,7 +5,7 @@ export class Age extends React.Component {
         return (
             <>
                 <p>Welcome, {this.props.name}!</p>
-                {this.props.age >= 18 && <p>Your Age is {this.props.age}</p> }
+                {this.props.age >= 18 && this.props.age <= 65 && this.props.name === 'John' && <p>Your Age is {this.props.age}</p> }
             </>
         );
     }
