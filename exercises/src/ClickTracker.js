b82daf5fe8f509cab16f96328ba7625571c3f837
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-09-10 19:27:08 +0200

    Events_04 done

--- exercises/src/ClickTracker.js
+++ exercises/src/ClickTracker.js
@@ -8,7 +8,7 @@ export class ClickTracker extends React.Component {
     eventHandler = (event) => {
         this.setState((state) => {
             return {
-                button: (state.button = event.target.innerText),
+                button: (state.button = event.target.text),
             }
         })
     }
@@ -16,11 +16,24 @@ export class ClickTracker extends React.Component {
     render() {
         return (
              <>
-                <button onClick={this.eventHandler}>BUTTON 1</button>
-                <button onClick={this.eventHandler}>BUTTON 2</button>
-                <button onClick={this.eventHandler}>BUTTON 3</button>
+                <button onClick={this.eventHandler}>
+                     <img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Perspective-Button-Stop-icon.png" width={200} alt="" />
+                </button>
+                <button onClick={this.eventHandler}>
+                     <img src="https://upload.wikimedia.org/wikipedia/commons/c/c9/Perspective-Button-Go-icon.png" width={200} alt="" />
+                </button>
+                <button onClick={this.eventHandler}>
+                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Button_Icon_Yellow.svg/1200px-Button_Icon_Yellow.svg.png" width={200} alt="" />
+                </button>
                 <h1>LAST BUTTON PRESSED IS: {this.state.button}</h1>
              </>
         );
     }
 }
+
+// EVENTS_04 CI SONO DIVERSE SOLUZIONI, SI PUò AGGIUNGERE IL TESTO MA CON L'AGGIUNTA DI UN EVENTO ONCLICK SUL TAG <IMG>... COSì SI PUò ANDARE A FARE UN EVENTO CON TARGET DEL TESTO FUORI DAL TAG <IMG>.
+// EVENTS_04 CI SONO DIVERSE SOLUZIONI, SI PUò AGGIUNGERE IL TESTO MA CON L'AGGIUNTA DI UN EVENTO ONCLICK SUL TAG <IMG>... COSì SI PUò ANDARE A FARE UN EVENTO CON TARGET DEL TESTO FUORI DAL TAG <IMG>.
+// EVENTS_04 CI SONO DIVERSE SOLUZIONI, SI PUò AGGIUNGERE IL TESTO MA CON L'AGGIUNTA DI UN EVENTO ONCLICK SUL TAG <IMG>... COSì SI PUò ANDARE A FARE UN EVENTO CON TARGET DEL TESTO FUORI DAL TAG <IMG>.
+// EVENTS_04 CI SONO DIVERSE SOLUZIONI, SI PUò AGGIUNGERE IL TESTO MA CON L'AGGIUNTA DI UN EVENTO ONCLICK SUL TAG <IMG>... COSì SI PUò ANDARE A FARE UN EVENTO CON TARGET DEL TESTO FUORI DAL TAG <IMG>.
+// EVENTS_04 CI SONO DIVERSE SOLUZIONI, SI PUò AGGIUNGERE IL TESTO MA CON L'AGGIUNTA DI UN EVENTO ONCLICK SUL TAG <IMG>... COSì SI PUò ANDARE A FARE UN EVENTO CON TARGET DEL TESTO FUORI DAL TAG <IMG>.
+// EVENTS_04 CI SONO DIVERSE SOLUZIONI, SI PUò AGGIUNGERE IL TESTO MA CON L'AGGIUNTA DI UN EVENTO ONCLICK SUL TAG <IMG>... COSì SI PUò ANDARE A FARE UN EVENTO CON TARGET DEL TESTO FUORI DAL TAG <IMG>.
