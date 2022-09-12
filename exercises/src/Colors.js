ce1a4413d927b8a18c5ada6ea488ad2ec4a39ec1
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-09-12 19:26:06 +0200

    Lists_02 done

--- exercises/src/Colors.js
+++ exercises/src/Colors.js
@@ -4,8 +4,7 @@ export class Colors extends React.Component {
     render() {
         return (
              <ul> LISTA COLORI
-                {this.props.items.map((item, index) =>
-                    <li key={item + index}>{item}</li>)}
+                {this.props.items.map(item => <li key={item.id}>{item.name}</li>)}
              </ul>
         );
     }
