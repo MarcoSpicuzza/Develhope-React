8f3614833c791dd225cc90962353a2d740d25fbd
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-09-14 10:20:11 +0200

    Styling components 1

--- exercises/src/Colors.js
+++ exercises/src/Colors.js
@@ -2,8 +2,13 @@ import React from "react";
 
 export class Colors extends React.Component {
     render() {
+        const addBgBorder = {
+            backgroundColor: 'maroon',
+            border: '3px solid black',
+        }
+
         return (
-             <ul> LISTA COLORI
+             <ul className={this.props.className} style={addBgBorder}> LISTA COLORI
                 {this.props.items.map(item => <li key={item.id}>{item.name}</li>)}
              </ul>
         );
--- exercises/src/index.js
+++ exercises/src/index.js
@@ -6,7 +6,7 @@ import { TodoList } from './TodoList';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Colors items={[{name: "Rosso", id: 1}, {name: "Verde", id: 2}, {name: "Arancione", id: 3}, {name: "Bianco", id: 5}]}/>
+    <Colors className='color' items={[{name: "Rosso", id: 1}, {name: "Verde", id: 2}, {name: "Arancione", id: 3}, {name: "Bianco", id: 5}]}/>
     <TodoList />
   </React.StrictMode>
 );
