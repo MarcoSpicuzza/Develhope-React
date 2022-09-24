9242f7dce78527a753c22e6c288c3277fe4b1d0c
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-09-24 20:28:35 +0200

    context 1

---
+++ exercises/src/App.js
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import DisplayLanguage from "./DisplayLanguage";
+
+const App = () => {
+    const LanguageContext = createContext('it');
+    const [language, setLanguage] = useState();
+
+    function changeLanguage(e) {
+        setLanguage(e.target.value)   
+    }
+
+    return ( 
+        <div>
+            <select name="language" onChange={changeLanguage}>
+                <option value="it">ITALIANO</option>
+                <option value="en">ENGLISH</option>
+            </select>
+            <LanguageContext.Provider value={language}>
+                <DisplayLanguage />
+            </LanguageContext.Provider>
+        </div>
+     );
+}
+ 
+export default App;
---
+++ exercises/src/DisplayLanguage.js
@@ -0,0 +1,10 @@
+const DisplayLanguage = () => {
+    return ( 
+        <div>
+            <p>Benvenuto utente!</p>
+            <p>Welcome user!</p>
+        </div>
+     );
+}
+ 
+export default DisplayLanguage;
