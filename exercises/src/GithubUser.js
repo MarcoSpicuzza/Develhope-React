4b4901dfaa26bac3fd08da1c1aa1bdf5589e89c2
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-09-28 17:00:21 +0200

    push

--- exercises/src/GithubUser.js
+++ exercises/src/GithubUser.js
@@ -1,14 +1,14 @@
 import UseGithubUser from "./UseGithubUser";
 
 const GithubUser = ({username}) => {
-    const {user} = UseGithubUser(username);
+    const {user, loading, error} = UseGithubUser(username);
 
     return (
         <div>
             <h1>GITHUB PROFILE</h1>
-            <h3>{user.login}</h3>
-            <h3>{user.id}</h3>
-            <h3>{user.url}</h3>
+            {error ? "Ci sono dei problemi" : loading ? <h1>Caricamento...</h1> : <h3> {user.login} </h3>}
+            {error ? "Ci sono dei problemi" : loading ? <h1>Caricamento...</h1> : <h3> {user.id} </h3>}
+            {error ? "Ci sono dei problemi" : loading ? <h1>Caricamento...</h1> : <h3> {user.url} </h3>}
         </div>
      );
 }
--- exercises/src/UseGithubUser.js
+++ exercises/src/UseGithubUser.js
@@ -2,14 +2,27 @@ import { useEffect, useState } from "react";
 
 const UseGithubUser = (username) => {
     const [user, setUser] = useState(username);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(`https://api.github.com/users/${username}`)
-        .then(response => response.json())
-        .then(data => setUser(data))
-    }, [username]);
+        (async () => {
+            setLoading(true)
+            const res = await fetch(`https://api.github.com/users/${username}`)
+            if (!res.ok) {
+                setError(true)
+                setLoading(false)
+            } else {
+                const data = await res.json()
+                setUser(data)
+            }
+            setLoading(false)
+        })()
+    }, [username])
 
-    return {user}
+    return { 
+        user, loading, error
+    }
 }
  
 export default UseGithubUser;
