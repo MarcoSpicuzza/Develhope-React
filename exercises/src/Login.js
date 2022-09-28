6c03f47990f6efc97856c5266a7aa6e455f8ae74
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-09-28 18:33:26 +0200

    pushing

--- exercises/src/Login.js
+++ exercises/src/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Login = () => {
     const [form, setForm] = useState({
@@ -7,6 +7,12 @@ const Login = () => {
         remember: false,
         login: false
     })
+
+    const inputRef = useRef();
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
 
     function handleInputChange(e) {
         const {name, type, value, checked} = e.target
@@ -25,7 +31,7 @@ const Login = () => {
 
     return ( 
         <div>
-            <input type="text" name="username" value={form.username} onChange={handleInputChange} />
+            <input ref={inputRef} type="text" name="username" value={form.username} onChange={handleInputChange} />
             <input type="password" name="password" value={form.password} onChange={handleInputChange}/>
             <input type="checkbox" name="remember"checked={form.remember} onChange={handleInputChange}/>
             {form.username && form.password ? <button name="login" onClick={onLogin}>LOGIN</button> : <button name="login" disabled>LOGIN</button>}
