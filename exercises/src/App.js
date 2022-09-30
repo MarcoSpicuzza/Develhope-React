3ec2429f84b999e7857612609dcc23e156025d86
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-09-30 19:09:27 +0200

    pushing

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import Counter from "./Counter";
 import Welcome from "./Welcome";
 import ShowGithubUser from "./ShowGithubUser";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 const App = () => {
     return ( 
         <div>
             <BrowserRouter>
+                <div>
+                    <Link to="/">HOME </Link>&nbsp;
+                    <Link to="/Counter">COUNTER</Link>&nbsp;
+                    <Link to="/users/marcospicuzza">GITHUB</Link>&nbsp;
+                </div>
                 <Routes>
                     <Route path='/' element={<Welcome name="Marco"/>} />
                     <Route path="/counter" element={<Counter initialValue={2} increment={2} />} />
