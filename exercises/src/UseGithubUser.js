8e8799efb4c0c8f0a25dfbf25e90de4bd1b490de
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-09-30 21:02:14 +0200

    pushing

--- exercises/src/UseGithubUser.js
+++ exercises/src/UseGithubUser.js
@@ -3,8 +3,10 @@ import useSWR from 'swr'
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const UseGithubUser = (username) => {
-    const { data } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
 
+    if (error) return "An error has occurred.";
+    if (!data) return "Loading..."
     return {
         user: data,
     }
