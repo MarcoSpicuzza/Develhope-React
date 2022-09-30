2dae2e605659df81a5ee7529e172601a433252dd
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-09-30 21:08:02 +0200

    pushing

--- exercises/src/GithubUser.js
+++ exercises/src/GithubUser.js
@@ -1,10 +1,11 @@
 import UseGithubUser from "./UseGithubUser";
 
 const GithubUser = ({username}) => {
-    const {user} = UseGithubUser(username);
+    const {user, onClickFetch} = UseGithubUser(username);
 
     return (
         <div>
+            <button onClick={onClickFetch}>FETCH DATA</button>
             <h1>GITHUB PROFILE</h1>
             {user && <h3>Username: {user.login}</h3>}
             {user && <p>Bio: {user.bio}</p>}
--- exercises/src/UseGithubUser.js
+++ exercises/src/UseGithubUser.js
@@ -3,12 +3,17 @@ import useSWR from 'swr'
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const UseGithubUser = (username) => {
-    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    const { data, mutate, error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+
+    const onClickFetch = () => {
+        mutate();
+    }
 
     if (error) return "An error has occurred.";
     if (!data) return "Loading..."
     return {
         user: data,
+        onClickFetch
     }
 }
  
