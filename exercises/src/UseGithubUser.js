954dc82a3b3bdb6c60858159e0f3e84bf7a2717d
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-10-04 18:34:34 +0200

    added null on UseGithubUser.js Line: 6

--- exercises/src/UseGithubUser.js
+++ exercises/src/UseGithubUser.js
@@ -3,7 +3,7 @@ import useSWR from 'swr'
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const UseGithubUser = (username) => {
-    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    const { data, error } = useSWR(() => username ? `https://api.github.com/users/${username}` : null, fetcher)
 
     if (error) return "An error has occurred.";
     if (!data) return "Loading..."
